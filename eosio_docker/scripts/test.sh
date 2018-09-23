d287be77313a440c55cc03c2a3f8aa4bec63020d
Hugo Roussel <hugo.roussel@epfl.ch>
2018-09-23 05:41:10 +0100

    update function fix and test script

---
+++ eosio_docker/scripts/test.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+scripts/deploy_contract.sh satellite satelliteacc satellitewal $(cat satellite_wallet_password.txt
+cleos push action satelliteacc add '["satelliteacc", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf", "dix", "onze", "douze", "satelliteacc"]' -p satelliteacc@active)
+cleos get table satelliteacc satelliteacc employee
+cleos push action satelliteacc update '["satelliteacc", "dan", "vitalik", "satoshi", "quatre", "cinq", "six", "sept", "huit", "neuf", "dix", "onze", "douze", "sat"]' -p satelliteacc@active
